0149c949989faf95ee5b93935c19df77d9d45328
kamusarihanshi <ahmedizaka9@gmail.com>
2024-03-25 11:38:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const myCatsArray = [...cats, name];
+    return myCatsArray;
+}
+
+function prependCat(name){
+    const myCatsArray = [name, ...cats];
+    return myCatsArray;
+}
+
+function removeLastCat(){
+    const newCatsArray = cats.slice(0, -1);
+    return newCatsArray;
+}
+
+function removeFirstCat(){
+    const newCatsArray = cats.slice(1);
+    return newCatsArray;
+}
